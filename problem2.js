96379cba50faa0d6d6bcbb2e6655d01664611a00
ddileepp <157563361+ddileepp@users.noreply.github.com>
2026-02-10 18:02:42 +0530

    Refactor getHobbiesByAge to use filter and flatMap

--- problem2.js
+++ problem2.js
@@ -1,13 +1,11 @@
 function getHobbiesByAge(peopleData, age) {
-  let hobbies = [];
-
-  for (let i = 0; i < peopleData.length; i++) {
-    if (peopleData[i].age === age) {
-      hobbies.push(peopleData[i].hobbies);
-    }
-  }
-
-  return hobbies;
+  return peopleData
+    .filter(function (person) {
+      return person.age === age;
+    })
+    .flatMap(function (person) {
+      return person.hobbies;
+    });
 }
 
 module.exports = getHobbiesByAge;
